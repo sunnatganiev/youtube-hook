190b47d07df399f1d9ce0afb4b53a52d4e93d6b7
sunnatganiev <sunnatganiev09@gmail.com>
2021-06-24 10:35:54 +0500

    deploy uchun tayyor

---
+++ src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('')
+
+  const onInputChange = (e) => {
+    setTerm(e.target.value)
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    onFormSubmit(term);
+  };
+
+  return (
+    <div className="ui segment search-bar">
+      <form className="ui form" onSubmit={onSubmit}>
+        <div className="field">
+          <label htmlFor="">Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={onInputChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
